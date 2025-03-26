7c5f1b3df7d620917a26ded4e5c012cdce7ce058
Thomas Kosciuch <thomas@qwealth.com>
2025-03-26 13:53:43 -0400

    added scheme for table

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 `npm run dev` will run dev (lol) from inside the front-end folder.
 
 `cargo run` will run the api
+
+`cargo test` will run all tests
+`cargo test --test test_conn` will test test_conn
--- partner-onboarding-rust-api/src/db.rs
+++ partner-onboarding-rust-api/src/db.rs
@@ -1,19 +1,16 @@
 use diesel::mysql::MysqlConnection;
-use diesel::r2d2::{self, ConnectionManager};
+use diesel::r2d2::{ConnectionManager, Pool};
+use dotenv::dotenv;
 use std::env;
 
-pub type DbPool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
-
-pub fn establish_connection() -> DbPool {
-    dotenv::dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    let manager = ConnectionManager::<MysqlConnection>::new(database_url);
-    r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.")
-}
+pub type DbPool = Pool<ConnectionManager<MysqlConnection>>;
 
 pub fn init_db() -> DbPool {
-    establish_connection()
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<MysqlConnection>::new(database_url);
+    Pool::builder()
+        .max_size(1000)
+        .build(manager)
+        .expect("Failed to create database pool")
 }
